eb992ec3d75e8e6216e69fa4c5d7456eb5566147
Damnik Jain <jaindamnik203@gmail.com>
2019-07-20 21:03:27 +0530

    updated requirements.txt

--- web/models.py
+++ web/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Customer(models.Model):
+	Customer_id = models.AutoField(primary_key=True, max_length=50, blank=False)
+	def __str__(self):
+		return str(self.Customer_id)
+
+
+class UserDetails(models.Model):
+	user = models.OneToOneField(User,on_delete=models.CASCADE,related_name='profile')
+	customer = models.OneToOneField(Customer, on_delete=models.CASCADE, default=1)
