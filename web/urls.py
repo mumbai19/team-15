20cb2590fa1c9b14eb106eb068e3d774a04a2bd3
Damnik Jain <jaindamnik203@gmail.com>
2019-07-21 01:40:01 +0530

    Login & Register

--- web/urls.py
+++ web/urls.py
@@ -5,8 +5,10 @@ app_name = 'web'
 
 urlpatterns = [
 
+	path('index/', views.index, name="index"),
 
-	path('register/', views.login_user(), name='register'),
-	path('login/', views.register(), name='login'),
-	path('logout/', views.logoutForm, name='logout'),
+	path('register/', views.RegisterFormView.as_view(), name='register'),
+	path('login/', views.LoginFormView.as_view(), name='login'),
+	path('logout/', views.logoutForm, name='logout')	
+
 ]
--- web/views.py
+++ web/views.py
@@ -4,47 +4,132 @@ from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth import logout
 from django.views.generic.edit import CreateView,UpdateView,DeleteView
-from django.core.urlresolvers import reverse_lazy
-from .forms import UserForm
-
+from .forms import LoginForm, RegisterForm
 IMAGE_FILE_TYPES = ['png', 'jpg', 'jpeg']
+from django.http import JsonResponse
+from django.contrib.auth.models import User
+def index(request):
+	return JsonResponse({'hello':"world"})
 
-def login_user(request):
-    if request.method == "POST":
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            if user.is_active:
-                login(request, user)
-                return render(request, 'web/index.html')
-            else:
-                return render(request, 'web/login.html', {'error_message': 'Your account has been disabled'})
-        else:
-            return render(request, 'web/login.html', {'error_message': 'Invalid login'})
-    return render(request, 'web/login.html')
+class RegisterFormView(View):
+	form_class = RegisterForm
+	template_name = 'web/register.html'
+
+	def get(self, request):
+		print('Get request on register form')
+		form = self.form_class(None)
+		error, err_email = '', ''
+		try:
+			error = request.session.get('err_mess', '')
+			err_email = request.session.get('err_email', '')
+			del request.session['err_email']
+			del request.session['err_mess']
+		except:
+			pass
+		print(error+" ")
+
+		return render(request, self.template_name, {'cart_size' : request.session.get('cart_size', 0),'form':form, 'err_email': err_email, 'register_error': error})
 
 
-def register(request):
-    form = UserForm(request.POST or None)
-    if form.is_valid():
-        user = form.save(commit=False)
-        username = form.cleaned_data['username']
-        password = form.cleaned_data['password']
-        user.set_password(password)
-        user.save()
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            if user.is_active:
-                login(request, user)
-                return render(request, 'web/index.html')
-    context = {
-        "form": form,
-    }
-def logout_user(request):
-    logout(request)
-    form = UserForm(request.POST or None)
-    context = {
-        "form": form,
-    }
-    return render(request, 'web/login.html', context)
+	def post(self, request):
+		form = self.form_class(request.POST)
+		print(str(form.is_valid()))
+		print(str(form.errors))
+		if "already exists" in str(form.errors):
+			request.session['err_email'] = request.POST.get('username', '')
+			request.session['err_mess'] = "duplicate"
+			return redirect('/register/')
+
+		if form.is_valid():
+			print('Valid register form')
+			user = form.save(commit=False)
+			print("cleaning data")
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password']
+
+			acount = 0
+			for cr in username:
+				if cr == "@":
+					acount += 1
+
+			if acount != 1:
+				request.session['err_mess'] = "invalidemail"
+				return redirect('/register/')
+
+
+			# gender = request.POST['gender']
+
+			prevuser = User.objects.filter(email=username)
+			print(str(prevuser.count())+' count of user')
+			if prevuser.count()>0:
+				print('duplicate email')
+				request.session['err_mess'] = "duplicate"
+				return redirect('/register/')
+			
+			user.set_password(password)
+			user.email = username
+			user.save()
+			user = authenticate(username=username, password=password)
+
+			if user is not None:
+				print(user.is_active)
+				if user.is_active:
+					print("loggin user")
+					login(request, user)
+					print('REdirect check for login')
+					return redirect('web:index')
+				else:
+					print('The user acccount is disabled')
+					return redirect('/login/?login=disabled')
+			else:
+				return redirect('/login/?login=failed')
+				print('The username and password is wrong')
+		
+
+
+class LoginFormView(View):
+	form_class = LoginForm
+	template_name = 'web/login.html'
+	def get(self, request):
+		error = request.GET.get('login_error', '')
+		if str(request.GET.get('m')) == '3' :
+			error = 'login_to_checkout'
+
+		form = self.form_class(None)
+		print("Error Value = "+error)
+		return render(request, self.template_name, {'cart_size' : request.session.get('cart_size', 0),'form':form, 'login_error': error })
+
+
+	def post(self, request):
+		print('received an post request for login')
+		form = self.form_class(request.POST)
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(username=username, password=password)
+		useremail = User.objects.filter(email=username)
+
+		if user is not None:
+			if user.is_active:
+				login(request, user)
+				# return redirect('pgs:index')
+				print('REdirect check for login')
+				return redirect('web:index')
+			else:
+				print('The user acccount is disabled')
+				return redirect('/login?login_error=disabled')
+		else:
+			print('The username and password is wrong')
+			if useremail.count()>0:
+				return redirect('/login?login_error=incorrect')
+			else:
+				return redirect('/login?login_error=failed')
+		
+
+
+from django.contrib.auth import logout
+
+def logoutForm(request):
+	logout(request)
+	print("logout called, user logged out")
+	# Redirect to a success page.
+	return redirect('web:index')
