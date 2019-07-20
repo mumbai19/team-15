257d8dbabfca877dd997d9a391d6f3144d667fd8
Raj Madheshia <rajsmadheshia@gmail.com>
2019-07-20 23:43:56 +0530

    Setting Changes

--- trishul/settings.py
+++ trishul/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'trishul',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306',
     }
