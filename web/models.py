ff5ce1273e7333ac67375c1c789ce8f15cc23c4e
Damnik Jain <jaindamnik203@gmail.com>
2019-07-20 21:41:37 +0530

    commit

--- web/models.py
+++ web/models.py
@@ -2,12 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Customer(models.Model):
-	Customer_id = models.AutoField(primary_key=True, max_length=50, blank=False)
-	def __str__(self):
-		return str(self.Customer_id)
+# class Customer(models.Model):
+# 	Customer_id = models.AutoField(primary_key=True, max_length=50, blank=False)
+# 	def __str__(self):
+# 		return str(self.Customer_id)
 
+# class UserDetails(models.Model):
+# 	user = models.OneToOneField(User,on_delete=models.CASCADE,related_name='profile')
+# 	customer = models.OneToOneField(Customer, on_delete=models.CASCADE, default=1)
 
-class UserDetails(models.Model):
-	user = models.OneToOneField(User,on_delete=models.CASCADE,related_name='profile')
-	customer = models.OneToOneField(Customer, on_delete=models.CASCADE, default=1)
+# class 
